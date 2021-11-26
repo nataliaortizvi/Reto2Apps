8b1dcfe02c4c575d11b2e0364478ce2d55f63aa0
nataliaortizvi <natiorvi00@gmail.com>
2021-11-25 23:19:36 -0500

    Initial commit

---
+++ app/src/main/java/com/example/reto2apps/PokemonActivity.java
@@ -0,0 +1,14 @@
+package com.example.reto2apps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PokemonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon3);
+    }
+}
